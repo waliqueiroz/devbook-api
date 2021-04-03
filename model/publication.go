c125eaad1f4df4e193caaa07a981b70d536a07ce
Wali Queiroz <wsantos077@gmail.com>
2021-04-03 14:03:33 -0300

    chore: publication model and table

---
+++ model/publication.go
@@ -0,0 +1,16 @@
+package model
+
+import (
+	"time"
+)
+
+// Publication represents a publication made by a user
+type Publication struct {
+	ID         uint64    `json:"id,omitempty"`
+	Title      string    `json:"title,omitempty"`
+	Content    string    `json:"content,omitempty"`
+	AuthorID   uint64    `json:"author_id,omitempty"`
+	AuthorNick string    `json:"author_nick,omitempty"`
+	Likes      uint64    `json:"likes"`
+	CreatedAt  time.Time `json:"created_at,omitempty"`
+}
--- resource/sql/sql.sql
+++ resource/sql/sql.sql
@@ -3,6 +3,7 @@ CREATE DATABASE IF NOT EXISTS devbook;
 USE devbook;
 
 DROP TABLE IF EXISTS followers;
+
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users(
@@ -20,4 +21,14 @@ CREATE TABLE followers (
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
     FOREIGN KEY (follower_id) REFERENCES users(id) ON DELETE CASCADE,
     primary key (user_id, follower_id)
+) ENGINE = INNODB;
+
+CREATE TABLE publications(
+    id int auto_increment primary key,
+    title varchar(255) not null,
+    content text not null,
+    author_id int not null,
+    likes int not null default 0,
+    created_at timestamp default current_timestamp()
+    FOREIGN KEY (author_id) REFERENCES users(id) ON DELETE CASCADE,
 ) ENGINE = INNODB;
